0651f3be236c7673527acde968dfb392caba8b71
V CHANDRA SEKHAR REDDY <68380005+vennapusac@users.noreply.github.com>
2021-03-31 10:15:15 +0530

    Service Discovery changes

--- src/main/java/net/com/springmvc/MainApp.java
+++ src/main/java/net/com/springmvc/MainApp.java
@@ -2,8 +2,10 @@ package net.com.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MainApp {
    public static void main(String[] args) {
 	   SpringApplication app = new SpringApplication(MainApp.class);
